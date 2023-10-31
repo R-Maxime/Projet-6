1b8950fdb629fab896cf91fe44c75128bec62ccb
R-Maxime <styleshoot34@gmail.com>
2023-10-31 14:41:41 +0100

    Add 404 CSS

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,7 +1,13 @@
+import { NavLink } from "react-router-dom"
+import '../../styles/Error.scss'
+
+
 function NotFound() {
   return (
-    <div>
+    <div className="error">
       <h1>404</h1>
+      <p>Oups! La page que vous demandez n&apos;existe pas.</p>
+      <NavLink className="link" to="/">Retourner sur la page d&apos;accueil</NavLink>
     </div>
   )
 }
