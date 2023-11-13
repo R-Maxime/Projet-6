f00eed7259af994e26a7214701f65a1aaaf8ea83
R-Maxime <styleshoot34@gmail.com>
2023-11-13 15:26:19 +0100

    Remove useState and onClick handlers in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,23 @@
-import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import Logo from '../assets/logo_colored.png';
 import '../styles/Header.scss';
 
 export default function Header() {
-  const setActive = useState('')[1];
-
-  const handleLinkClick = (link) => {
-    setActive(link);
-  };
-
   return (
     <header className='kasa-header'>
-      <NavLink to='/' exact='true'>
+      <NavLink to='/'>
         <img src={Logo} alt="Logo de Kasa" className='kasa-logo' />
       </NavLink>
       <nav className='kasa-nav'>
         <NavLink
           to='/'
           className='kasa-nav-item'
-          onClick={() => handleLinkClick('accueil')}
         >
           Accueil
         </NavLink>
         <NavLink
           to='/about'
           className='kasa-nav-item'
-          onClick={() => handleLinkClick('about')}
         >
           À propos
         </NavLink>
